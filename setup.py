ab454411847c999bd19580f0bef32a4fa13e4f85
Muhammad Adil Naeem <162784706+muhammadadilnaeem@users.noreply.github.com>
2024-11-01 14:56:04 +0500

    setup file for this project

---
+++ setup.py
@@ -0,0 +1,11 @@
+
+from setuptools import find_packages, setup
+
+setup(
+    name="us_visa_project",
+    version="0.0.1",
+    author="Adil",
+    description="A Project for US Visa Approval Prediction",
+    author_email="madilnaeem0@gmail.com",
+    packages=find_packages(),
+)

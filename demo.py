f57586b0585055a9f1d6b2fa4c4422c8f3dbe550
Muhammad Adil Naeem <162784706+muhammadadilnaeem@users.noreply.github.com>
2024-11-01 17:41:51 +0500

    demo purpose for checking codes

--- demo.py
+++ demo.py
@@ -4,3 +4,17 @@
 # from us_visa_project.logger import logging
 
 # logging.info("Hello World! Let's Start the project")
+
+
+## demo to check if our custom excemption class is working
+
+# import sys
+# from us_visa_project.exception import USVISAException
+# from us_visa_project.logger import logging
+
+# logging.info("We got the error in our code, let's handle the project")
+
+# try:
+#     a = 1/0
+# except Exception as e:
+#     raise USVISAException(e,sys)

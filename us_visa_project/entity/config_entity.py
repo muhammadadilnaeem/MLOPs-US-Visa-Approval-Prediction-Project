0095c96306169a810bfb0cdc0854099834d78931
Muhammad Adil Naeem <162784706+muhammadadilnaeem@users.noreply.github.com>
2024-11-03 09:30:04 +0500

    implement training pipeline and data ingestion configuration with timestamped directories

--- us_visa_project/entity/config_entity.py
+++ us_visa_project/entity/config_entity.py
@@ -35,3 +35,14 @@ class DataIngestionConfig:
     train_test_split_ratio: float = DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO
     # Name of the database collection used for storing visa data
     collection_name: str = DATA_INGESTION_COLLECTION_NAME
+
+
+# Define a class to hold configuration settings for data validation
+@dataclass
+class DataValidationConfig:
+    # Directory path for storing data validation artifacts, set to a subdirectory within the main artifact directory
+    data_validation_dir: str = os.path.join(training_pipeline_config.artifact_dir, DATA_VALIDATION_DIR_NAME)
+    
+    # File path for the data drift report, which combines the data validation directory, report directory, and file name
+    drift_report_file_path: str = os.path.join(data_validation_dir, DATA_VALIDATION_DRIFT_REPORT_DIR,
+                                               DATA_VALIDATION_DRIFT_REPORT_FILE_NAME)

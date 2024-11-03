d0913f4f9da22569ee9ca662d9dd414ce313fbe5
Muhammad Adil Naeem <162784706+muhammadadilnaeem@users.noreply.github.com>
2024-11-03 09:31:02 +0500

    define DataIngestionArtifact data class to store paths for training and testing data files

--- us_visa_project/entity/artifact_entity.py
+++ us_visa_project/entity/artifact_entity.py
@@ -9,3 +9,14 @@ class DataIngestionArtifact:
     trained_file_path: str
     # Path to the testing data file
     test_file_path: str
+
+
+@dataclass
+class DataValidationArtifact:
+
+    # Status of data validation
+    validation_status:bool
+    # Message for data validation
+    message: str
+    # Path to the drift report
+    drift_report_file_path: str

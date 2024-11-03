e471ffbf7fbc7b101d912c97e772da3960afe819
Muhammad Adil Naeem <162784706+muhammadadilnaeem@users.noreply.github.com>
2024-11-03 12:32:00 +0500

    define DataIngestionArtifact data class to store paths for training and testing data files

--- us_visa_project/entity/artifact_entity.py
+++ us_visa_project/entity/artifact_entity.py
@@ -2,7 +2,6 @@
 from dataclasses import dataclass
 
 # Class representing the data ingestion artifacts
-
 @dataclass
 class DataIngestionArtifact:
     # Path to the training data file
@@ -10,7 +9,7 @@ class DataIngestionArtifact:
     # Path to the testing data file
     test_file_path: str
 
-
+# Class representing the data Validation artifacts
 @dataclass
 class DataValidationArtifact:
 
@@ -20,3 +19,54 @@ class DataValidationArtifact:
     message: str
     # Path to the drift report
     drift_report_file_path: str
+
+# Class representing the data Transformation artifacts
+@dataclass
+class DataTransformationArtifact:
+
+    # Path to the transformed object file
+    transformed_object_file_path:str 
+    # Path to the transformed train file
+    transformed_train_file_path:str
+    # Path to the transformed test file
+    transformed_test_file_path:str
+
+# Class representing the model trainer artifacts
+@dataclass
+class ClassificationMetricArtifact:
+    # Accuracy of the model
+    f1_score:float
+    # Precision of the model
+    precision_score:float
+    # Recall of the model
+    recall_score:float
+
+# Class representing the model trainer artifacts
+@dataclass
+class ModelTrainerArtifact:
+    # Path to the trained model
+    trained_model_file_path:str 
+    # Metric artifact
+    metric_artifact:ClassificationMetricArtifact
+
+
+# Class representing the model evaluation artifacts
+@dataclass
+class ModelEvaluationArtifact:
+    # Status of model evaluation
+    is_model_accepted:bool
+    # Metric artifact to compare model performance
+    changed_accuracy:float
+    # Path to the trained model for deployment
+    s3_model_path:str 
+    # Path to the trained model for deployment
+    trained_model_path:str
+
+
+# Class representing the model pusher artifacts
+@dataclass
+class ModelPusherArtifact:
+    # amazon s3 bucket name
+    bucket_name:str
+    # Path to the trained model for deployment
+    s3_model_path:str
